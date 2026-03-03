91bf4c88cb030f0f3e6e99301b845f72fba82d8e
SebasDavid14 <sebasdavid0211@gmail,com>
2026-03-03 15:59:40 -0500

    AGREGADO feature/ingresos

---
+++ model/Rol.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Rol {
+    private String nombreRol;
+
+    public Rol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
+}
