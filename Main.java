0bb46896ef7138dde392c6250632561323a3541e
SebasDavid14 <sebasdavid0211@gmail,com>
2026-03-03 16:10:39 -0500

    AGREGADO menu

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== SISTEMA FINANZAS FAMILIARES ===");
+
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("1. Registrar ingreso");
+            System.out.println("2. Registrar gasto");
+            System.out.println("3. Salir");
+            opcion = sc.nextInt();
+        } while(opcion != 3);
     }
 }
