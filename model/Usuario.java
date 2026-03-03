11c640d704aec413b068ee0e86028f654bf6d27f
SebasDavid14 <sebasdavid0211@gmail,com>
2026-03-03 15:57:05 -0500

    AGREGADO feature usuario

---
+++ model/Usuario.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Usuario {
+    private String nombre;
+    private String rol;
+
+    public Usuario(String nombre, String rol) {
+        this.nombre = nombre;
+        this.rol = rol;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getRol() { return rol; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setRol(String rol) { this.rol = rol; }
+
+    @Override
+    public String toString() {
+        return "Usuario: " + nombre + " - Rol: " + rol;
+    }
+}
